3aa0cfc3059d7352068f070744418b84d63b3670
rhinnen <rory@hinnen.us>
2024-04-29 09:06:17 -0500

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "swiftmodule-swift.h"
+
+int main(int argc, const char * argv[]) {
+    
+    // auto it = SwiftModule::SwiftIt::init("Hello Swift");
+    
+    // it.printSwift();
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ src/module.swift
@@ -0,0 +1,24 @@
+//
+//  SwiftModule.swift
+//  Module
+//
+//  Created by Rory on 3/23/24.
+//
+
+import Foundation
+
+public class SwiftIt {
+    
+    public init(it: String = "") {
+        self.it = it
+    }
+    
+    public var it = ""
+
+    private var that = ""
+
+    public func printSwift() {
+        print(it)
+    }
+
+}
